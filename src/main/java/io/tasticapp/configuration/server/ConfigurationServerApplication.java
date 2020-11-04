5623d59b25cb0ca520c01734bc35d52142f532c1
Laurent <dev.laurent.desiree@gmail.com>
2020-11-04 22:06:07 +0000

    typo and pom file fixes

--- src/main/java/io/tasticapp/configuration/server/ConfigurationServerApplication.java
+++ src/main/java/io/tasticapp/configuration/server/ConfigurationServerApplication.java
@@ -1,6 +1,13 @@
 package io.tasticapp.configuration.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 @EnableAutoConfiguration
 @EnableConfigServer
 public class ConfigurationServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigurationServerApplication.class, args);
+  }
 }
